49a8efbc0c6de0467de5b43cad62624c55940d41
Neal Groothuis <neal@troops.ai>
2021-12-02 18:03:33 -0500

    Day 01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,25 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var numIncreases = 0
+        for (i in 0..(input.size - 2)) {
+            if (input[i].toInt() < input[i + 1].toInt())
+                numIncreases++
+        }
+        return numIncreases
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var numIncreases = 0
+        for (i in 0..(input.size - 4)) {
+            if (input[i].toInt() < input[i + 3].toInt())
+                numIncreases++
+        }
+        return numIncreases
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part2(testInput) == 5)
 
     val input = readInput("Day01")
     println(part1(input))
